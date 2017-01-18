bbc7e5e368a10f580967db7aacc2b86fe624ccdf
joshua <jrinard@hotmail.com>
2017-01-17 16:55:34 -0800

    add favorites

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,8 +9,8 @@ import { Component } from '@angular/core';
     <h1>Recipe Box</h1>
     <h3>{{category}}</h3>
       <ul> <!-- repeater DIRECTIVE --> <!-- tasks is the array and it is assigning each iteration to currentRecipe temporarly -->
-        <li [class]="priorityColor(currentRecipe)"
-           (click)="isDone(currentRecipe)"
+        <li [class]="preferredColor(currentRecipe)"
+           (click)="isFav(currentRecipe)"
            *ngFor="let currentRecipe of recipeArray">{{currentRecipe.title}}, {{currentRecipe.ingredients}}, {{currentRecipe.directions}}
            <button class="btn btn-xs" (click)="editRecipe(currentRecipe)">Edit</button>
         </li><!-- assigning li tag to a loop/repeater -->
@@ -18,14 +18,14 @@ import { Component } from '@angular/core';
       <hr>
       <div *ngIf="selectedRecipe"> <!-- ngIF checks selectedRecipe's value of null. if it is not null due to clickedRecipe then it shows-->
         <h3>{{selectedRecipe.title}}</h3>
-        <p>Recipe Complete? {{selectedRecipe.done}}</p>
+        <p>Favorite: {{selectedRecipe.favorite}}</p>
         <h3>Edit Recipe</h3>
         <label>Enter Recipe Name:</label>
         <input class="form-control" [(ngModel)]="selectedRecipe.title"><!-- ngModel = event and property binding// assigning input to description // provides a shortcut for two-way data binding in forms-->
         <br>
-        <input type="radio" [(ngModel)]="selectedRecipe.priority" [value]="1"> 1 (Low Priority)<br><!-- radio buttons require predefined values to select from. -->
-        <input type="radio" [(ngModel)]="selectedRecipe.priority" [value]="2"> 2 (Medium Priority)<br>
-        <input type="radio" [(ngModel)]="selectedRecipe.priority" [value]="3"> 3 (High Priority)<br>
+        <input type="radio" [(ngModel)]="selectedRecipe.preferred" [value]="1"> Green (Favorite Preference)<br><!-- radio buttons require predefined values to select from. -->
+        <input type="radio" [(ngModel)]="selectedRecipe.preferred" [value]="2"> Blue (Average Preference)<br>
+        <input type="radio" [(ngModel)]="selectedRecipe.preferred" [value]="3"> Red (Dislike Preference)<br>
         <button class="btn btn-xs" (click)="finishedEditing()">Close Edit</button><!-- When clicked changes selectedRecipe to null to hide -->
       </div>
   </div>
@@ -52,21 +52,21 @@ export class AppComponent {
     this.selectedRecipe = null;
   }
 
-  isDone(clickedRecipe: Recipe) {
-    if(clickedRecipe.done === true) {
-      // alert("Done");
+  isFav(clickedRecipe: Recipe) {
+    if(clickedRecipe.favorite === true) {
+      // alert("Favorite");
     } else {
-      // alert("NOT Done");
+      // alert("Not Favorite");
     }
   }
 
-  priorityColor(currentRecipe){
-    if (currentRecipe.priority === 3){
+  preferredColor(currentRecipe){
+    if (currentRecipe.preferred === 3){
       return "bg-danger";
-    } else if (currentRecipe.priority === 2) {
-      return "bg-warning";
-    } else {
+    } else if (currentRecipe.preferred === 2) {
       return "bg-info";
+    } else {
+      return "bg-success";
     }
   }
 
@@ -74,6 +74,6 @@ export class AppComponent {
 
 //class declaration is our MODEL which holds our data // CONSTRUCTOR
 export class Recipe {
-  public done: boolean = false;
-  constructor(public title: string, public ingredients: string, public directions: string, public priority: number) { }
+  public favorite: boolean = false;
+  constructor(public title: string, public ingredients: string, public directions: string, public preferred: number) { }
 }
